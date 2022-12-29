0c11b81d0ed1d30366e2929dfdc17023bd3e5326
kishorekumarcodingmart <kishorekumar@codingmart.com>
2022-12-29 10:06:25 +0530

    Navbar Completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import NavbarLogo from '../../Asset/Images/navLogo.svg'
-import ApartmentsLogo from '../../Asset/Images/apartments.svg'
 import ThreeDots from '../../Asset/Images/threeDots.svg'
+// import Flag from '../../Asset/Images/flag.png'
+import { navbarData } from './data'
 import './Navbar.css'
 
 function Navbar() {
@@ -14,64 +15,61 @@ function Navbar() {
         </div>
         <div className='otherLeftOption'>
           <ul className='listOfOtherLeftOption'>
-
-            <li>
-              <div className='option'>
-                <div className='redTagMemu'>
-                  <span>Bundle and save!</span>
-                </div>
-                <Link className='menuLink'>Flight + Hotel</Link>
-              </div>
-            </li>
-
-            <li>
-              <div className='option'>
-                <Link className='menuLink'>Hotels & Homes</Link>
-              </div>
-            </li>
-
-            <li>
-              <div className='option'>
-                <Link className='menuLink'>Flights</Link>
-              </div>
-            </li>
-
-            <li>
-              <div className='option'>
-                <div className='redTagMemu'>
-                  <span>New!</span>
-                </div>
-                <Link className='menuLink'>Coupons & Deals</Link>
-              </div>
-            </li>
-
-            <li>
-              <div className='option '>
-                <span className='menuWithIcon'>
-                  <img src={ApartmentsLogo} alt="ApartmentsLogo"/>
-                </span>
-                <Link className='menuLink'>Apartments</Link>
-              </div>
-            </li>
-
-            <li>
-              <div className='option'>
-                <div className='redTagMemu'>
-                  <span>New!</span>
-                </div>
-                <Link className='menuLink'>Things to do</Link>
-              </div>
-            </li>
+            {
+              navbarData.map((val, index) => {
+                return (
+                  (val.type === "tag") ? (<li key={index}>
+                    <div className='option'>
+                      <div className='redTagMemu'>
+                        <span>{val.tag}</span>
+                      </div>
+                      <Link className='menuLink'>{val.link}</Link>
+                    </div>
+                  </li>) : (val.type === "nonTag") ? (
+                    <li key={index}>
+                      <div className='option'>
+                        <Link className='menuLink'>{val.link}</Link>
+                      </div>
+                    </li>) : (
+                    <li key={index}>
+                      <div className='option '>
+                        <span className='menuWithIcon'>
+                          <img src={val.src} alt={val.alt} />
+                        </span>
+                        <Link className='menuLink'>{val.link}</Link>
+                      </div>
+                    </li>
+                  )
+                )
+              })
+            }
 
             <li>
               <div className='option '>
                 <span className='menuWithIcon'>
-                  <img src={ThreeDots} alt="ApartmentsLogo"/>
+                  <img src={ThreeDots} alt="ApartmentsLogo" />
                 </span>
               </div>
             </li>
 
           </ul>
+        </div>
+      </div>
+      <div className='rightNavbarWrapper'>
+        <div className='buttonWrapper'>
+          <button className='listYourPlaceBtn'>List your place</button>
+        </div>
+        <div className='countryOption'>
+            <img className='countryOptionImg' src="https://cdn6.agoda.net/images/mobile/flag-us@2x.png" alt="Flag" />
+        </div>
+        <div className='currency'>
+          <span>Rs.</span>
+        </div>
+        <div className='loginSignupBtnWrapper'>
+            <div className="loginSignupBtnHolder">
+              <button className='loginBtn'>Sign in</button>
+              <button className='signupBtn'>Create account</button>
+            </div>
         </div>
       </div>
     </nav>
