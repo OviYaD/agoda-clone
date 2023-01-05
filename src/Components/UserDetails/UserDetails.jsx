88016be491b55c0eaa7c75c0d40e0e0c66c4b8c5
kishorekumarcodingmart <kishorekumar@codingmart.com>
2023-01-05 18:48:46 +0530

    userDetails

--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
+import { reducerFunction } from '../../Helper/Reducer'
+import { useNavigate } from 'react-router-dom'
 import EmailComponent from './EmailComponent'
 import NameComponent from './NameComponent'
 import Newsletter from './Newsletter'
@@ -13,28 +15,62 @@ import './UserDetails.css'
 
 function UserDetails() {
 
+  const navigate = useNavigate();
+
+  const INITIAL_STATE = {
+    loading: false,
+    data: {},
+    error: false
+  }
+
+  const [state, dispatch] = useReducer(reducerFunction, INITIAL_STATE)
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_START" })
+    fetch(`${process.env.REACT_APP_API_KEY}/profile`, {
+      method: "POST",
+      body: {},
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: data })
+        if (!data.status) {
+          navigate('/profile', { replace: true })
+        }
+      })
+      .catch(()=>{
+        dispatch({type : "FETCH_ERROR"})
+      })
+  }, [navigate])
+
+
+  console.log(state)
 
   const [paymentInfo, setPaymentInfo] = useState(false)
 
   return (
     <section className='userDetailsWrapper'>
-        <h1>User details</h1>
-          <NameComponent />
-          <EmailComponent />
-          <PhoneNumber />
-          <PasswordComponent />
-          <section className='paymentMethods'>
-            <h1>Payment methods</h1>
-          </section>
-          <Payment paymentInfo={paymentInfo} />
-          <SaveCredit paymentInfo={paymentInfo} setPaymentInfo={setPaymentInfo} />
-          <section className='paymentMethods'>
-            <h1>Email subscriptions</h1>
-          </section>
-          <Newsletter />
-          <Reminders />
-          <Promotions />
-          <Offers />
+      <h1>User details</h1>
+      <NameComponent />
+      <EmailComponent />
+      <PhoneNumber />
+      <PasswordComponent />
+      <section className='paymentMethods'>
+        <h1>Payment methods</h1>
+      </section>
+      <Payment paymentInfo={paymentInfo} />
+      <SaveCredit paymentInfo={paymentInfo} setPaymentInfo={setPaymentInfo} />
+      <section className='paymentMethods'>
+        <h1>Email subscriptions</h1>
+      </section>
+      <Newsletter />
+      <Reminders />
+      <Promotions />
+      <Offers />
     </section>
   )
 }
