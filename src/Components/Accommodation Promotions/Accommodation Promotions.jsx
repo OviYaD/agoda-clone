765401b85bbb68de79f6cd71da56d28910f31b97
kishorekumarcodingmart <kishorekumar@codingmart.com>
2023-01-11 13:59:54 +0530

    Accommodation Promotions

---
+++ src/Components/Accommodation Promotions/Accommodation Promotions.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Accommodation Promotions.css'
+
+function AccommodationPromotions() {
+  return (
+    <div className='apWrapper'>
+        <div className='apT'>
+            <div></div>
+            <h3>Accommodation Promotions</h3>
+            <section>
+                <span className='apViewAll'>
+                    <span>View all</span>
+                    <span>{" >"}</span>    
+                </span>
+            </section>
+        </div>
+    </div>
+  )
+}
+
+export default AccommodationPromotions
--- src/Components/HomeBody/HomeBody.jsx
+++ src/Components/HomeBody/HomeBody.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import AccommodationPromotions from '../Accommodation Promotions/Accommodation Promotions'
 import './HomeBody.css'
 
 function HomeBody() {
   return (
     <section className='homePageWrapper'>
-        hello
+        <AccommodationPromotions />
     </section>
   )
 }
